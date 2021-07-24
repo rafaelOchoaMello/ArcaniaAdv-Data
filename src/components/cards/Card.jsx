067bf76a330148afd785e2ce8b8abfeda03f3722
rafaelOchoaMello <ochoagames2@gmail.com>
2021-07-24 12:03:38 -0300

    :tada: Iniciando Projeto Arcania Adventures Database

---
+++ src/components/cards/Card.jsx
@@ -0,0 +1,30 @@
+//Rafael Ochôa Mello - rafaelochoamello@gmail.com
+//23-Julho-2021
+
+/* Este componente deve servir tanto para monstros quanto 
+para NPCS, posteriormente será adicionado um condicional
+que deve definir a localização (folder) da imagem 
+a ser carregada */
+
+import React from 'react';
+import './Card.css';
+
+function Card({name, level, race, hp, mp, image}) {
+    return (
+        <>
+            <div className="cardContainer">
+                <div className="cardHeader">
+                    <p>{name}</p>
+                </div>
+                <div className="cardBody">
+                    <img src={window.location.origin + `/assets/monsters/${image}`} alt="" />
+                    <p> <strong>{level}</strong> </p>
+                    <p>{race}</p>
+                    <p>{hp}/{mp}</p>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Card;
---
+++ src/screens/MonstersScreens.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Card from '../components/cards/Card';
+import MonsterData from '../data/monsters.json';
+
+import './MonsterScreen.css';
+
+function MonsterScreen(props) {
+    return (
+        <div className="monsterScreen">
+            {
+                MonsterData.map((monster) => {
+                    return <Card key={monster.id}
+                        name={monster.name}
+                        level={monster.level}
+                        race={monster.race}
+                        hp={monster.hp}
+                        mp={monster.mp}
+                        image={monster.image}
+                    />
+                })
+            }
+
+        </div>
+    );
+}
+
+export default MonsterScreen;
