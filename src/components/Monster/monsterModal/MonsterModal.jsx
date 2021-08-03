ad64d2083cebd357b60c956a6eb3940a6f67ad4d
rafaelOchoaMello <ochoagames2@gmail.com>
2021-08-03 03:49:15 -0300

    :package: Add Element on monster.json, fix sidebar width

--- src/components/Monster/monsterModal/MonsterModal.jsx
+++ src/components/Monster/monsterModal/MonsterModal.jsx
@@ -13,6 +13,9 @@ function MonsterModal(props) {
                     <div className="monsterModal-content">
                         <h1>{props.data.name}</h1>
                         <img src={window.location.origin + `/assets/monsters/${props.data.image}`} alt="" />
+                        <p>{props.data.level}</p>
+                        <p>{props.data.race}</p>
+                        <p>{props.data.element}</p>
                         <p>Dano-F:{props.data.damage} | Defesa-F:{props.data.defense}</p>
                         <p>Dano-M:{props.data.magicDamage} | Defesa-M:{props.data.magicDefense}</p>
                     </div>
