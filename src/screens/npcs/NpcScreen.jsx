691765cbc90102ac16a3324fb63f89ef0478715b
rafaelOchoaMello <ochoagames2@gmail.com>
2021-08-11 19:52:47 -0300

    :package: Add npc.json data

--- src/screens/npcs/NpcScreen.jsx
+++ src/screens/npcs/NpcScreen.jsx
@@ -1,11 +1,14 @@
 import './NpcScreen.css';
 import React from 'react';
+
 import CardNpc from '../../components/npc/card/CardNpc';
+import NpcData from '../../data/npc.json'
 
 function NpcScreen(props) {
+    
     return (
         <>
-            <CardNpc/>
+            {console.log(NpcData)}
         </>
     );
 }
